46f7f7a4a7098ae9dbf634ae06a4491a973f1f3d
Shota MINEGISHI <smngs@mine-313.com>
2019-08-25 21:34:37 +0000

    [add] backup.sh, deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,19 +4,26 @@ set -u
 DOT_DIRECTORY="${HOME}/dotfiles"
 DOT_CONFIG_DIRECTORY=".config"
 
-echo "link home directory dotfiles"
+# Backup and Link home directory dotfiles.
 cd ${DOT_DIRECTORY}
 for f in .??*
 do
     [ "$f" = ".git" ] && continue
     [ "$f" = "bin" ] && continue
     [ "$f" = ".config" ] && continue
+
     ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
 done
 
-echo "link .config directory dotfiles"
+# Link .config directory dotfiles.
 cd ${DOT_DIRECTORY}/${DOT_CONFIG_DIRECTORY}
-for file in `\find . -maxdepth 8 -type f`; do
+for file in `\find . -maxdepth 1 -type d`; do
+    # make .config directory if not exists.
+    if [ ! -e "${HOME}/${DOT_CONFIG_DIRECTORY}" ]; then
+    	mkdir "${HOME}/${DOT_CONFIG_DIRECTORY}"
+    fi
+
+    [ "$file" = ".config" ] && continue
     ln -snfv ${DOT_DIRECTORY}/${DOT_CONFIG_DIRECTORY}/${file:2} ${HOME}/${DOT_CONFIG_DIRECTORY}/${file:2}
 done
 
