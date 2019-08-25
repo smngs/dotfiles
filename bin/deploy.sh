40e689da234252d2c898079409a82c209e96ade8
Shota MINEGISHI <smngs@mine-313.com>
2019-08-25 22:07:25 +0000

    [update] deploy.sh (environment dependent)

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,13 +4,14 @@ set -u
 DOT_DIRECTORY="${HOME}/dotfiles"
 DOT_CONFIG_DIRECTORY=".config"
 
-# Backup and Link home directory dotfiles.
+# Link home directory dotfiles.
 cd ${DOT_DIRECTORY}
 for f in .??*
 do
     [ "$f" = ".git" ] && continue
     [ "$f" = "bin" ] && continue
     [ "$f" = ".config" ] && continue
+    [ "$f" = ".Xresources" ] && continue
 
     ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
 done
@@ -24,8 +25,26 @@ for file in `\find . -maxdepth 1 -type d`; do
     fi
 
     [ "$file" = ".config" ] && continue
+    [ "$file" = "i3" ] && continue
+    [ "$file" = "polybar" ] && continue
+
     ln -snfv ${DOT_DIRECTORY}/${DOT_CONFIG_DIRECTORY}/${file:2} ${HOME}/${DOT_CONFIG_DIRECTORY}/${file:2}
 done
 
-echo "linked dotfiles complete!"
+# Link home directory dotfiles and .config directory dotfiles which is enviroment dependent.
+case `hostname -s` in
+    "arcturus") 
+        ln -snfv ${DOT_DIRECTORY}/arcturus/.Xresources ${HOME}/.Xresources
+        ln -snfv ${DOT_DIRECTORY}/arcturus/i3 ${HOME}/${DOT_CONFIG_DIRECTORY}/i3
+        ln -snfv ${DOT_DIRECTORY}/arcturus/polybar ${HOME}/${DOT_CONFIG_DIRECTORY}/polybar
+    ;;
+    "spica") 
+        ln -snfv ${DOT_DIRECTORY}/spica/.Xresources ${HOME}/.Xresources
+        ln -snfv ${DOT_DIRECTORY}/spica/i3 ${HOME}/${DOT_CONFIG_DIRECTORY}/i3
+        ln -snfv ${DOT_DIRECTORY}/spica/polybar ${HOME}/${DOT_CONFIG_DIRECTORY}/polybar
+    ;;
+    *)
+    ;;
+esac
 
+echo "linked dotfiles complete!"
