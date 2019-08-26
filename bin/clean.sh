60a5a293161b891bee6e36a34a48da6f165c26d8
Shota MINEGISHI <smngs@mine-313.com>
2019-08-26 09:04:00 +0900

    [fix] clean.sh

--- bin/clean.sh
+++ bin/clean.sh
@@ -13,19 +13,17 @@ do
     [ "$f" = ".config" ] && continue
     [ "$f" = "host" ] && continue
 
-    rm -rf ${HOME}/${f}
+    rm -rf "${HOME}/${f}"
 done
 
 # Delete .config directory dotfiles.
 cd ${DOT_DIRECTORY}/${DOT_CONFIG_DIRECTORY}
 for file in `\find . -maxdepth 1 -type d`; do
-    [ "$file" = ".config" ] && continue
-
-    rm -rf ${HOME}/${DOT_CONFIG_DIRECTORY}/${file:2}
+    rm -rf "${HOME}/${DOT_CONFIG_DIRECTORY}/${file:2}"
 done
 
 rm -rf ${HOME}/.Xresources
 rm -rf ${HOME}/${DOT_CONFIG_DIRECTORY}/i3
 rm -rf ${HOME}/${DOT_CONFIG_DIRECTORY}/polybar
 
-echo "linked dotfiles complete!"
+echo "Deleted dotfiles complete!"
