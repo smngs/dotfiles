8a2f8bae885ab550abe3f37639633a465805e414
Shota MINEGISHI <smngs@mine-313.com>
2020-06-28 20:16:44 +0900

    [add] .hyper.js

--- .hyper.js
+++ .hyper.js
@@ -9,10 +9,11 @@ module.exports = {
     updateChannel: 'stable',
 
     // default font size in pixels for all tabs
-    fontSize: 11,
+    fontSize: 13,
 
     // font family with optional fallbacks
-    fontFamily: 'SauceCodePro Nerd Font Mono',
+    // fontFamily: 'SauceCodePro Nerd Font Mono',
+    fontFamily: 'Cica',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
