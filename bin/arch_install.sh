93326699df82312a89c6f8629b1ab2a6b41485d0
Shota MINEGISHI <smngs@mine-313.com>
2022-07-17 17:56:36 +0900

    [fix] ci settings and related files

--- bin/arch_install.sh
+++ bin/arch_install.sh
@@ -1,8 +1,12 @@
-sudo pacman -Syu
-mkdir /tmp/yay
-cd /tmp/yay
-curl -OJ 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay'
-makepkg -si
-cd
-rm -rf /tmp/yay
-yay --version
+#!/bin/bash
+
+if [[ $(uname -r) =~ ARCH$ ]]; then
+    sudo pacman -Syu --noconfirm
+    mkdir /tmp/yay
+    cd /tmp/yay || exit
+    curl -OJ 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay'
+    makepkg -si --noconfirm
+    cd || exit
+    rm -rf /tmp/yay
+    yay --version
+fi
