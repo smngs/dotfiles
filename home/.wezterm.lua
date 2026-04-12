2466a515f6a4c4372fe047ea8d8fd5abb2133c74
Shota Minegishi <smngs@smngs.io>
2026-04-12 21:57:03 +0900

    [update] migrate terminal emulator from Alacritty to WezTerm

---
+++ home/.wezterm.lua
@@ -0,0 +1,21 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.color_scheme = "iceberg-dark"
+config.font = wezterm.font("CodeM Nerd Font")
+config.font_size = 14
+config.cell_width = 0.81
+config.ime_preedit_rendering = "System"
+config.enable_tab_bar = false
+config.default_prog = { "/bin/zsh", "-l", "-c", "zellij attach --index 0 --create" }
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+config.window_close_confirmation = "NeverPrompt"
+config.initial_cols = 160
+config.initial_rows = 48
+
+return config
